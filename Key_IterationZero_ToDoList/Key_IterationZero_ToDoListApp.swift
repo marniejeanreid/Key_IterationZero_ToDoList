2cf4632133247a0175445bf773bfa1147c0dfad4
Marnie Reid <marnie@kodewithklossy.com>
2023-06-20 11:09:27 -0700

    Initial Commit

---
+++ Key_IterationZero_ToDoList/Key_IterationZero_ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Key_IterationZero_ToDoListApp.swift
+//  Key_IterationZero_ToDoList
+//
+//  Created by scholar on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Key_IterationZero_ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
